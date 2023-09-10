203a7d33c1a64aeadac5f093dd241de3b9aac49b
Mercan Hesenova <mercan_hesenova@mail.ru>
2023-09-10 21:11:27 +0400

    responsiv duzelisler

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let link = document.getElementsByClassName("pagination__link");
-link.addEventListener('click', (e) => {
-  link.classList.remove("is_active");
-  this.classList.add("is_active");
-});
+const links = document.getElementsByClassName("pagination__link"); 
+const linksArray = Array.from(links); 
+linksArray.forEach(link => { link.addEventListener('click', function(e) { linksArray.forEach(link => { link.classList.remove("is_active"); }); 
+this.classList.add("is_active"); }); });
